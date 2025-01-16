2072d0aaa3ce03fdd2a4d732d4b00fa450e16563
Cambio1 <uspwolf2563@gmail.com>
2025-01-16 08:55:07 -0500

    COMMIT - LOGIN

---
+++ POO_Grupo5/RECUPERACION/LOGIN.java
@@ -0,0 +1,16 @@
+package RECUPERACION;
+
+import javax.swing.*;
+
+public class LOGIN {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private String[] opciones = {"Administrador", "Usuario"};
+    private JComboBox <String> comboBox1= new JComboBox<>(opciones);;
+
+    public LOGIN() {
+
+    }
+
+
+}
